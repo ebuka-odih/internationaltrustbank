7a075455b5768d6c19c1bda6076bdbbb848394f7
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-22 18:17:18 +0100

    commit changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('convert', function ($money) {
             return "<?php echo number_format($money, 2); ?>";
         });
+        Schema::defaultStringLength(191);
+
     }
 }
